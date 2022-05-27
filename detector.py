395d93bd52c0680bb3fdffddf782675ca45ec2f0
VladTomici14 <vladtomici@gmail.com>
2022-05-27 22:07:17 +0300

    Started to write the code for the shape detector.

---
+++ detector.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+class Detector:
+    def __init__(self):
+
+        pass
+
+    def extract_shape(self):
+
+
+
+    def detect_color(self, image):
+        # TODO: build a color detector from scratch (maybe c++)
+        pass
