fcd860433a3fc9355635aa0e901b74a97154b8ce
VladTomici14 <vladtomici@gmail.com>
2022-05-28 14:59:37 +0300

    Implemented the layers of the model. Started to connect the database to the model.

--- train.py
+++ train.py
@@ -1,19 +1,82 @@
-from tensorflow.keras.layers import Dense, Dropout, Convolution2D, MaxPooling2D, Flatten
+from tensorflow.keras.layers import BatchNormalization, Dense, Reshape, MaxPooling2D, Flatten
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 import tensorflow
+import numpy as np
+import h5py
+import cv2
 
-colors_labels = ["red", "green", "blue"]
-shape_labels = ["triangles", "rectangles", "ellipses"]
+n_imgs = 100
 
-filter_size = 3
-pool_size = 2
+classes = {0: "-",
+           1: "rectangleR",
+           2: "rectangleG",
+           3: "rectangleB",
+           4: "ellipseR",
+           5: "ellipseG",
+           6: "ellipseB",
+           7: "triangleR",
+           8: "triangleG",
+           9: "triangleB"}
 
-img_size = (96, 96)
+# --------- loading the data set -------
+# dataset_name = "images_10k_10000bb.h5"
+# xy_h5py = h5py.File(f"images/{dataset_name}", "r")
+#
+# x = np.array(xy_h5py["x_train"])
+# y = np.array(xy_h5py["y_train"])
+# images_mean = np.array(xy_h5py["imgs_mean"])
+# images_std = np.array(xy_h5py["imgs_std"])
+
+# del x, y
+#
+# xy_h5py.close()
+
+# ------ declaring some fine tuning parameters for the model --------
+# n_examples = x.shape[0]
+# side_dim = x.shape[1]
+# n_bb = y.shape[1] // 5
+n_classes = 10
+img_size = (96, 96, 3)
+base_model_name = "DenseNet201"
+
+# --------- declaring the model, based on DenseNet -------
+base_model = tensorflow.keras.applications.DenseNet121(input_shape=img_size,
+                                                       include_top=False)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+# ------- adding more layers ------
+
+base_model_input = base_model.output
+model = Sequential()
+model.add(base_model)
+
+# flatten = Flatten()(base_model)
+# model.add(flatten)
+model.add(Flatten())
+
+# batch_normalization = BatchNormalization()(flatten)
+# model.add(batch_normalization)
+model.add(BatchNormalization())
+
+# class_categorical = Dense(10, activation="softmax")(batch_normalization)
+# model.add(class_categorical)
+model.add(Dense(10, activation="softmax"))
+
+# class_output = Reshape((n_bb, n_classes), name="class_output")(class_categorical)
+#
+# score_confidence = Dense((n_bb), name="score_confidence", activation="tanh")(batch_normalization)
+# score_coords = Dense((n_bb * 4), name="score_coords")(batch_normalization)
+
+model.summary()
+
+tensorflow.keras.utils.plot_model(model, "model_layers.png", True)
 
 # --------------- data set augmentation ---------
-train_data_generator = ImageDataGenerator(rescale=1. / 255,
-                                          rotation_range=5,
+# TODO: delete/edit for fine tuning
+train_data_generator = ImageDataGenerator(rotation_range=5,
                                           validation_split=0.2,
                                           width_shift_range=0.2,
                                           height_shift_range=0.2,
@@ -21,37 +84,24 @@ train_data_generator = ImageDataGenerator(rescale=1. / 255,
                                           horizontal_flip=True,
                                           fill_mode="nearest")
 
+valid_data_generator = ImageDataGenerator(rescale=1. / 255,
+                                          validation_split=0.2)
+
 # ---------- loading data set ----------
-training_set_path = "images/"
+training_set_path = "images/train"
 training_set = train_data_generator.flow_from_directory(training_set_path,
                                                         target_size=(96, 96),
                                                         batch_size=64,
                                                         class_mode="categorical",
                                                         subset="training")
 
-model = Sequential([
-    Convolution2D(32, 6, 6, input_shape=X.shape[1:], dim_ordering='tf', activation='relu'),
-    MaxPooling2D(pool_size=(pool_size, pool_size)),
-    Convolution2D(64, filter_size, filter_size, dim_ordering='tf', activation='relu'),
-    MaxPooling2D(pool_size=(pool_size, pool_size)),
-    Convolution2D(128, filter_size, filter_size, dim_ordering='tf', activation='relu'),
-    Convolution2D(128, filter_size, filter_size, dim_ordering='tf', activation='relu'),
-    Flatten(),
-    Dropout(0.4),
-    Dense(256, activation='relu'),
-    Dropout(0.4),
-    Dense(y.shape[-1])
-])
-
 # -------- metrics for the compilation --------
 METRICS = [
-        tensorflow.keras.metrics.BinaryAccuracy(name="accuracy"),
-        tensorflow.keras.metrics.Precision(name="precision"),
-        tensorflow.keras.metrics.Recall(name="recall"),
-        tensorflow.keras.metrics.AUC(name="auc")
+    tensorflow.keras.metrics.BinaryAccuracy(name="accuracy"),
+    tensorflow.keras.metrics.Precision(name="precision"),
+    tensorflow.keras.metrics.Recall(name="recall"),
+    tensorflow.keras.metrics.AUC(name="auc")
 ]
 
 # -------------- model compiling ---------
-model.compile(optimizer="adadelta",
-              loss="mse",
-              metrics=METRICS)
+# model.compile()
